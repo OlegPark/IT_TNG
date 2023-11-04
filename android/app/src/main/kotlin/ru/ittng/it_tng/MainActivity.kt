c27cb36f8db626e671b8b962e9a487a72dd7d675
Oleg <oleg.pak1996@gmail.com>
2023-11-04 22:45:05 +0300

    Первый коммит

---
+++ android/app/src/main/kotlin/ru/ittng/it_tng/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.ittng.it_tng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:it_tng/generated/l10n.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+  
+  final int _bonus = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              S.of(context).hello, 
+              style: TextStyle(fontSize: 32),
+            ),
+            Text(
+              S.of(context).goodbye, 
+              style: TextStyle(fontSize: 32),
+            ),
+            Text(
+              S.of(context).youReceiveNBonus(_bonus), 
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
