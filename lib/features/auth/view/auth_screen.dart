d4080b0003ed09f0d038da71df9ba4d569838d73
Oleg <oleg.pak1996@gmail.com>
2023-11-09 23:54:38 +0300

    +

---
+++ lib/features/auth/view/auth_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({super.key});
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login to your account'),
+      ),
+    );
+  }
+}
