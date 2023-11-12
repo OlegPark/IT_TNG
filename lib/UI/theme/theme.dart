a704051eadac86afc74f1187948af1e675c75879
Oleg <oleg.pak1996@gmail.com>
2023-11-12 14:23:49 +0300

    Добавить темы, свзяать страницу auth с main

---
+++ lib/UI/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+  primarySwatch: Colors.blueGrey,
+  dividerColor: Colors.white24,
+  appBarTheme: const AppBarTheme(
+    iconTheme: IconThemeData(color: Color.fromARGB(255, 136, 157, 224)),
+    elevation: 0,
+    backgroundColor: Color.fromARGB(255, 31, 31, 31),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+);
--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:it_tng/UI/theme/theme.dart';
 
 class AuthWidget extends StatefulWidget {
   const AuthWidget({super.key});
@@ -12,16 +13,6 @@ class _AuthWidgetState extends State<AuthWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [],
-              )
-            ],)
-            ),
-          ),
-      );
+      theme: darkTheme);
   }
 }
