9e8b569857b2cbe2d64050b87c5d6a4a26a14626
Oleg <oleg.pak1996@gmail.com>
2023-12-03 21:47:06 +0300

    +

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -6,22 +6,31 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/1BG.png"),
-            alignment: Alignment.topCenter
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/1BG.png"),
+                alignment: Alignment.topCenter
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 255, 255, 255),
+                  Color.fromARGB(255, 0, 87, 201),
+                ],
+              ),
+            ),
           ),
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 255, 255, 255),
-              Color.fromARGB(255, 0, 87, 201),
-              ]),
-        ),
-        
-      )
+          Container(
+            color: Colors.white,
+            height: 400,
+            width: double.infinity,
+          ),
+        ],
+      ),
     );
   }
 }
