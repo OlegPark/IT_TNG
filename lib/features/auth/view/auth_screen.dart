a8fa246aa097e015ec3659b85395324c6496221f
Oleg <oleg.pak1996@gmail.com>
2024-03-29 10:16:34 +0300

    +

--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -6,6 +6,7 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Expanded(
