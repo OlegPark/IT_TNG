56a96e71bc2d47dee3ec685930efc4c31d4c315e
Олег Пак <oleg.pak1996@gmail.com>
2024-09-30 23:47:35 +0300

    +

--- lib/features/auth/view/auth_screen.dart
+++ lib/features/auth/view/auth_screen.dart
@@ -47,65 +47,31 @@ class AuthScreen extends StatelessWidget {
               ),
               height: 450,
               width: 350,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 28.0, right: 28.0),
+              child: const Padding(
+                padding: EdgeInsets.only(left: 28.0, right: 28.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const TitleAuth(),
-                    const Text(
+                    TitleAuth(),
+                    Text(
                       'Система складского учёта для',
                       style: TextStyle(
                         color: Colors.grey,
                       ),
                     ),
-                    const Text(
+                    Text(
                       'IT-отделов',
                       style: TextStyle(
                         color: Colors.grey,
                       ),
                     ),
-                    const SizedBox(height: 30,),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Введите логин',
-                        border: OutlineInputBorder()
-                      ),
-                    ),
-                    const SizedBox(height: 30,),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Введите пароль',
-                        border: OutlineInputBorder()
-                      ),
-                    ),
+                    SizedBox(height: 30,),
+                    FormLogin(),
+                    SizedBox(height: 30,),
+                    FormPassword(),
                     SizedBox(height: 10,),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Text('Забыли пароль?', style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),),
-                    ),
                     SizedBox(height: 50,),
-                    Container(
-                      height: 60,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(193, 78, 7, 255), 
-                            Color.fromARGB(255, 0, 87, 201),
-                          ],
-                        ),
-                      ),
-                      child: Center(child: Text('Войти', style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),),),
-                    ),
+                    ButtonLogin(),
                   ],
                 ),
               ),
@@ -116,4 +82,3 @@ class AuthScreen extends StatelessWidget {
     );
   }
 }
-
--- lib/features/auth/widgets/wigets.dart
+++ lib/features/auth/widgets/wigets.dart
@@ -27,3 +27,63 @@ class TitleAuth extends StatelessWidget {
     );
   }
 }
+
+class ButtonLogin extends StatelessWidget {
+  const ButtonLogin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(193, 78, 7, 255), 
+            Color.fromARGB(255, 0, 87, 201),
+          ],
+        ),
+      ),
+      child: Center(child: Text('Войти', style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+        color: Colors.white,
+      ),),),
+    );
+  }
+}
+
+class FormPassword extends StatelessWidget {
+  const FormPassword({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Введите пароль',
+        border: OutlineInputBorder()
+      ),
+    );
+  }
+}
+
+class FormLogin extends StatelessWidget {
+  const FormLogin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Введите логин',
+        border: OutlineInputBorder()
+      ),
+    );
+  }
+}
