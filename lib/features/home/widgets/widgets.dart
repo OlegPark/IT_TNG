6fb21f64aea0737976d6b310d7b17291308f25ab
Олег Пак <oleg.pak1996@gmail.com>
2024-11-17 21:00:00 +0300

    +

---
+++ lib/features/home/widgets/widgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homewidgets extends StatelessWidget {
+  const Homewidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
